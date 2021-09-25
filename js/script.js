184c9e44ed30042d1ad5cce79de6d5b94d364a4c
Akshat Virmani <akshatvirmani93@gmail.com>
2021-09-25 15:11:00 +0530

    update

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
